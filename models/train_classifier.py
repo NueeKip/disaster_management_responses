b66a6c072b031acbaab380e6f81448902e9f0823
root <you@example.com>
2023-01-27 10:49:25 +0000

    Add Dosctrings to Train CLassifier

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,202 +1,131 @@
 import sys
-import re
-import pickle
+import nltk
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
-
-import nltk
-from nltk.corpus import stopwords
+import re
+nltk.download(['punkt', 'wordnet'])
 from nltk.tokenize import word_tokenize
-from nltk.stem.wordnet import WordNetLemmatizer
-
-
-from sklearn.metrics import classification_report
-from sklearn.multioutput import MultiOutputClassifier
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import GridSearchCV
+from nltk.stem import WordNetLemmatizer
+from sqlalchemy import create_engine
+from nltk.corpus import stopwords
+import re
+from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report
+from sklearn.tree import DecisionTreeClassifier
+import pickle
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-
-from sklearn.metrics import make_scorer, accuracy_score, f1_score, fbeta_score
-from scipy.stats import hmean
-from scipy.stats.mstats import gmean
-
-nltk.download(['punkt', 'wordnet','stopwords'])
-nltk.download(['averaged_perceptron_tagger'])
+from sklearn.metrics import precision_recall_fscore_support
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.filterwarnings('ignore')
 
 def load_data(database_filepath):
+    """Load the filepath and return the data
+       INPUT: database_filepath
+       OUTPUT: Dataframe
+               X: message column of dataframe as the input features
+               y: categories of the dataframe for ML classification
+               cate
     """
-        - Load cleaned data from database into dataframe
-        - Extract X, Y data
-        
-        Args:
-            database_filepath (str): File path of database
-            
-        Returns:
-            X (pandas dataframe): Contains messages
-            Y (pandas dataframe): Contains categories of disaster
-            category_names (list): Name of categories
-    """
-    # Load data from database
-    engine = create_engine('sqlite:///' + database_filepath)
-    #engine.dispose()
-    df = pd.read_sql_table("Disaster_Response", engine)
-    # Extract X and Y
-    X = df["message"]
-    Y = df.drop(["id", "message", "original", "genre"], axis=1)
-    category_names = Y.columns.tolist()
-    return X, Y, category_names
+    name = 'sqlite:///' + database_filepath
+    engine = create_engine(name)
+    df = pd.read_sql_table('Disaster_Response', con=engine) # is table always called this?
+    print(df.head())
+    X = df['message']
+    y = df[df.columns[4:]]
+    added = pd.get_dummies(df[['genre']])
+    y = pd.concat([y, added], axis=1)
+    return X, y
 
 def tokenize(text):
     """
-        - Tokenize text
-        
-        Args:
-            text (str): Message string
-            
-        Returns:
-            tokens (list): List of tokens
+    Function to tokenize and lemmatize the given text
+    INPUT: text message
+    OUTPUT:clean_tokens is a processed message
     """
-    #converting all the text to lowercase
-    text.lower()
-    # removing punctuation characters from the text and replacing them with an empty space
-    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
-    # use the word tokenizer to convert text into tokens
+    #remove punctuation and url links
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    #tokenize text
     tokens = word_tokenize(text)
-    # removing stopwords by calling a "for loop" for the tokens and using english stopwords from nltk!
-    tokens = [word for word in tokens if word not in stopwords.words("english")]
-    # Using lemmatization to strip all the words
-    tokens = [WordNetLemmatizer().lemmatize(word).strip() for word in tokens]
-    # finally return the tokens
-    return tokens
+    #remove stopwords
+    stop_words = stopwords.words("english")
+    tokens = [token for token in  tokens if token not in stop_words]
 
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-    """
-    Starting Verb Extractor class
-    
-    This class extract the starting verb of a sentence,
-    creating a new feature for the ML classifier
-    """
-
-    def starting_verb(self, text):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
-
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-
-
-def model_pipeline():
-    """
-        - Build pipeline for creating model
-        
-        Returns:
-            pipeline_modified: Pipeline for creating the model
-    """
-    # Build pipeline
-    pipeline_modified = Pipeline([
-            ('features', FeatureUnion([
-
-                ('text_pipeline', Pipeline([
-                    ('vect', CountVectorizer(tokenizer=tokenize)),
-                    ('tfidf', TfidfTransformer())
-                ])),
-
-                ('starting_verb', StartingVerbExtractor())
-            ])),
-
-            ('clf', RandomForestClassifier())
-        ])
-    return pipeline_modified
-
-
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
 
 def build_model():
-    """
-        - Build the model with the proposed parameters
-        
-        Returns:
-            cv (GridSearchCV): Machine learning model
-    """
-    model = model_pipeline()
-    # Parameter selection using GridSearchCV
-    parameters = {
-        'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
-        'features__text_pipeline__vect__max_df': (0.5, 0.75, 1.0),
-        'features__text_pipeline__vect__max_features': (None, 5000, 10000),
-        'features__text_pipeline__tfidf__use_idf': (True, False),
-        'clf__n_estimators': [50, 100, 200],
-        'clf__min_samples_split': [2, 3, 4],
-        'features__transformer_weights': (
-            {'text_pipeline': 1, 'starting_verb': 0.5},
-            {'text_pipeline': 0.5, 'starting_verb': 1},
-            {'text_pipeline': 0.8, 'starting_verb': 1},
-        )
-    }
-    cv = GridSearchCV(model, param_grid=parameters)
+    """Return Grid Search model with pipeline and Classifier"""
+    # Create pipeline with Classifier
+    moc = MultiOutputClassifier(RandomForestClassifier())
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', moc)
+        ])
+
+    parameters = {'clf__estimator__criterion': ['entropy'],
+              'clf__estimator__max_depth': [10, 50, None],
+             # 'clf__estimator__min_samples_leaf':[2, 5, 10],
+              'clf__estimator__n_estimators': [10,50,100]}
+
+    cv=GridSearchCV(pipeline, param_grid=parameters, cv=2, n_jobs=-1, verbose=1)
     return cv
-
-
-def evaluate_model(model, X_test, Y_test, category_names):
+def precision_recall_fscore(y_test,y_pred):
     """
-        - Evaluate model
-        
-        Args:
-            model (Predicter model): Machine learning model (Predicter)
-            X_test (pandas series): Test data set of X
-            Y_test (pandas dataframe): Test data set of Y
-            category_names (list): Name of categories
+    Function using  precision_recall_fscore_support provide better report
+    Input: y_test and y_pred
+    Output: prints the precision,recall,fscore report of each category
     """
-    # Predict test data
-    predict_y = model.predict(X_test)
-    
-    # Evaluate
-    for i in range(len(category_names)):
-        category = category_names[i]
-        print(category)
-        print(classification_report(Y_test[category], predict_y[:, i]))
+    report = pd.DataFrame(columns=['Category', 'f_score', 'precision', 'recall'])
 
+    for i,col in enumerate(y_test):
+        precision, recall, f_score, support = precision_recall_fscore_support(y_test[col], y_pred[:,i], average='weighted')
+        report.set_value(i, 'Category', col)
+        report.set_value(i, 'f_score', f_score)
+        report.set_value(i, 'precision', precision)
+        report.set_value(i, 'recall', recall)
+    print('simply the average results are:')
+    print('Mean f_score:', report['f_score'].mean())
+    print('Mean precision:', report['precision'].mean())
+    print('Mean recall:', report['recall'].mean())
+    return report
 
 def save_model(model, model_filepath):
-    """
-        - Saves model in pickle file
-        
-        Args:
-            model (Predicter model): Machine learning model (Predicter)
-            model_filepath (str): Path where model will save.
-    """
-    with open(model_filepath, 'wb') as file:  
-        pickle.dump(model, file)
-
+    """Save model as pickle file"""
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 def main():
+    """Load the data, run the model and save model"""
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+        X, y= load_data(database_filepath)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
-        model.fit(X_train, Y_train)
-        
+        model.fit(X_train.as_matrix(), y_train.as_matrix())
+        y_pred1 = model.predict(X_test)
+
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        report1 =precision_recall_fscore(y_test, y_pred1)
+        print(report1)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
